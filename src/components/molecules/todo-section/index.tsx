e301d0050de6ce344b34c1fe40ad2f5eb85c577d
tinnguyen <tinn1855@gmail.com>
2025-05-08 10:55:34 +0700

    fix mockAPI and add loading

--- src/components/molecules/todo-section/index.tsx
+++ src/components/molecules/todo-section/index.tsx
@@ -1,5 +1,6 @@
 import { TodoHeader } from '@/components/common/todo-header';
 import { TodoItem } from '@/components/common/todo-item';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useTodos } from '@/hooks/use-todos';
 
 export function TodoSection({
@@ -9,11 +10,11 @@ export function TodoSection({
 }) {
   const { todos, error, loading } = useTodos();
   const filteredTodos = todos.filter((todo) => todo.status === section);
-  if (loading) return <div className="p-4">Loading...</div>;
+  if (loading) return <Skeleton className="rounded-sm" />;
   if (error) return <div className="p-4 text-red-500">Erorr</div>;
 
   return (
-    <div className="bg-gray-100 h-screen p-4 mx-2 rounded-md">
+    <div className="bg-gray-100 h-screen p-4 mx-2 rounded-md col-span-1">
       <TodoHeader />
       <div className="flex-1 overflow-y-auto flex flex-col gap-2 mt-4">
         {filteredTodos.map((todo) => (
