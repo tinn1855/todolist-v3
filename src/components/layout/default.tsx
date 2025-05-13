d1ddf6519abaaf8e1f86e3485d6bc56e3c1e3d1f
tinnguyen <tinn1855@gmail.com>
2025-05-13 12:12:09 +0700

    fix sidebar

--- src/components/layout/default.tsx
+++ src/components/layout/default.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import { SidebarProvider, SidebarTrigger } from '../ui/sidebar';
+import { SidebarProvider } from '../ui/sidebar';
 import { AppSidebar } from '../molecules/sidebar';
 
 export function TemplateDefault() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarTrigger />
+
       <Outlet></Outlet>
     </SidebarProvider>
   );
--- src/components/molecules/sidebar/index.tsx
+++ src/components/molecules/sidebar/index.tsx
@@ -1,21 +1,40 @@
-import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react';
+import {
+  Calendar,
+  ChevronUp,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+  User2,
+} from 'lucide-react';
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarTrigger,
 } from '@/components/ui/sidebar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useState } from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 // Menu items.
 const items = [
   {
     title: 'Home',
-    url: '#',
+    url: '/',
     icon: Home,
   },
   {
@@ -41,8 +60,36 @@ const items = [
 ];
 
 export function AppSidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem className="flex items-center justify-between">
+            <div
+              className={`flex gap-2 items-center ${
+                isOpen ? 'hidden' : 'block'
+              } `}
+            >
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <h3 className="font-medium">Tin Nguyen</h3>
+            </div>
+            <SidebarTrigger
+              className={`flex duration-300 ease-in-out ${
+                isOpen ? 'justify-center' : 'justify-end'
+              }`}
+              onClick={handleToggleSidebar}
+            />
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
@@ -62,6 +109,34 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> tinn1855@gmail.com
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
